94b73a54464956b03e18b13c4000545f6460c108
Luca <72993234+Lunc4@users.noreply.github.com>
2022-11-15 21:56:28 +0100

    Update fixed_xor.py

--- fixed_xor.py
+++ fixed_xor.py
@@ -1,4 +1,4 @@
-# zipped invoer a en b en itereert vervolgens over alle bytes 
+# zipped invoer a en b en XORed vervolgens een voor een alle bytes 
 
 def xor_bytes(a:bytes,b:bytes) -> bytes:
     return bytes(x ^ y for x,y in zip(a,b))
